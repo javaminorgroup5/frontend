c3e0d11af529f1e478d8f38d5ce1cf9c3f58059a
Jurian Vink <jurianvnk@gmail.com>
2020-11-23 17:43:10 +0100

    boilerplating removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  {path: 'register', component: RegisterComponent },
+  {path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
