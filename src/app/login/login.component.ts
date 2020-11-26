91235f8a765e931e8e62063b97b74425493f3dd6
Jurian Vink <jurianvnk@gmail.com>
2020-11-26 09:35:00 +0100

    fi

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
+import { AuthService } from '../auth.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  loginForm;
+  login;
+  register;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService, private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: "",
+      password: "",
+    });
+    this.login = authService.login();
+    this.register = authService.register();
   }
 
+  ngOnInit(): void {}
+
+  onSubmit(event: any) {
+    console.log(event)
+  }
 }
