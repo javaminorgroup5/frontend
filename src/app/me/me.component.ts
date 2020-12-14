c746a7f42eb0fb65b4eb0682e2a41d84bd5e3b2e
wietzehiha <wietzehiha@gmail.com>
2020-12-14 18:07:28 +0100

    add recips to profile

--- src/app/me/me.component.ts
+++ src/app/me/me.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../profile.service';
 
+import { RecipeService } from '../recipe/service/recipe.service';
+import { Recipe } from '../recipe/model/recipe';
+
 @Component({
   selector: 'app-me',
   templateUrl: './me.component.html',
@@ -8,15 +11,48 @@ import { ProfileService } from '../profile.service';
 })
 export class MeComponent implements OnInit {
   profile: any;
+  recipes: Recipe[] = new Array();
 
-  constructor(private profileService: ProfileService) {}
+  constructor(
+      private profileService: ProfileService,
+      private recipeService: RecipeService
+  ) {}
 
   ngOnInit(): void {
+    this.loadRecipes();
     const userId = sessionStorage.getItem('userId');
     if (userId) {
       this.profileService.getUser(parseInt(userId, 2)).then((value) => {
         this.profile = value;
       });
+    }
+  }
+
+  async loadRecipes(): Promise<any>  {
+    const userId = sessionStorage.getItem('userId');
+    if (!userId) {
+      console.log('user id not found');
+      return;
+    }
+    const result: Recipe[] = await this.recipeService.getAllRecipesByUserId(parseInt(userId, 2));
+    if (result) {
+      for (const r of result) {
+        const recipe =
+            {
+              id: r.id,
+              recipe: r.recipe,
+              description: r.description,
+              title: r.title,
+              recipeImage:
+                  {
+                    type: r.recipeImage?.type,
+                    name: r.recipeImage?.name,
+                    picByte: 'data:image/jpeg;base64,' + r.recipeImage?.picByte
+                  }
+            };
+        this.recipes.push(recipe);
+      }
+      console.log(this.recipes);
     }
   }
 }
